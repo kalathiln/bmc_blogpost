9f051b4ec3f15676b5eab237ae28c2e5e47c2a7c
kalathiln <nikhkk@gmail.com>
2024-05-29 17:30:48 +0200

    Recieving form data using flask

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import datetime
 
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def home():
+    if request.method == "POST":
+        entry_content = request.form.get("content")
+        formatted_date = datetime.datetime.today().strftime("%Y-%M-%D")
+        print(entry_content, formatted_date)
     return render_template("home.html")
