b8bf658b8f3af74ec74222a80b9646144d7214e2
kalathiln <nikhkk@gmail.com>
2024-06-01 04:10:37 +0200

    Minor clean up after functional testing of the app.

--- app.py
+++ app.py
@@ -24,21 +24,21 @@ def home():
         entry_content = request.form.get("content")
         formatted_date = datetime.datetime.today().strftime("%d-%m-%Y")
         # print(entry_content, formatted_date)
-        entries.append((entry_content, formatted_date))
+        # entries.append((entry_content, formatted_date))
         app.db.entries.insert_one({"content" : entry_content, "date" : formatted_date})
 
-        # Now here we need to reformat the date and add it as additional formatting
-        entries_with_date = [
-            (
-                entry [0],
-                entry[1],
-                datetime.datetime.strptime(entry[1], "%d-%m-%Y").strftime("%b %d")
-            )
-            for entry in entries
-        ]
+    # Now here we need to reformat the date and add it as additional formatting
+    entries_with_date = [
+        (
+            entry ["content"],
+            entry["date"],
+            datetime.datetime.strptime(entry["date"], "%d-%m-%Y").strftime("%b %d")
+        )
+        for entry in app.db.entries.find({})
+    ]
 
 
-        kwargs = {
-            "entries" : entries_with_date
-        }
+    kwargs = {
+        "entries" : entries_with_date
+    }
     return render_template("home.html", **kwargs)
